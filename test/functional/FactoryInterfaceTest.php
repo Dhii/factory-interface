27674ab489918c582cb40e1423cbd822851787c2
Miguel Muscat <miguelmuscat93@gmail.com>
2017-04-02 13:28:53 +0200

    Added `FactoryInterface`

---
+++ test/functional/FactoryInterfaceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Dhii\Factory\FuncTest;
+
+use Dhii\Factory\FactoryInterface;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see Dhii\Factory\FactoryInterface}.
+ *
+ * @since [*next-version*]
+ */
+class FactoryInterfaceTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Factory\\FactoryInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return FactoryInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->make()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject, 'Subject is not a valid instance.'
+        );
+    }
+}
