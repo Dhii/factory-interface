8d7688ce32a55d24eb6ac4e0c3c7737ee945e39d
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-02 14:46:31 +0200

    Removed mentions of services and containers

--- src/FactoryInterface.php
+++ src/FactoryInterface.php
@@ -2,28 +2,22 @@
 
 namespace Dhii\Factory;
 
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
-
 /**
- * Something that can create service instances.
+ * Something that can create instances.
  *
- * @since 0.1
+ * @since [*next-version*]
  */
 interface FactoryInterface
 {
     /**
-     * Creates a new instance of a service.
+     * Creates a new instance.
      *
-     * @since 0.1
+     * @since [*next-version*]
      *
-     * @param string $id     The ID of the service to create.
+     * @param string $id     The ID of the instance to create.
      * @param mixed  $config Some kind of configuration.
      *
-     * @throws NotFoundExceptionInterface  If the given ID does not represent a known service.
-     * @throws ContainerExceptionInterface If an error occurred while resolving the service definition.
-     *
-     * @return object|null The created service instance or null if the given ID does not represent
+     * @return object|null The created instance or null on failure.
      */
     public function make($id, array $config = array());
 }
