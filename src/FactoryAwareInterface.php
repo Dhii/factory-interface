6096f0c7c48ac3f7a4d3abdc50278262fecc9cb6
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-01 17:23:49 +0100

    Added `FactoryAwareInterface`

---
+++ src/FactoryAwareInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dhii\Factory;
+
+/**
+ * Something that can have a factory retrieved from it.
+ *
+ * @since [*next-version*]
+ */
+interface FactoryAwareInterface
+{
+    /**
+     * Retrieves the factory associate with this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return FactoryInterface
+     */
+    public function getFactory();
+}
